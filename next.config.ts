ee28d941e594b51ad745932a0596f7b1d8b41ac2
AssoDePicche <samuelprado730@gmail.com>
2025-04-18 19:30:59 -0300

    feat: access control allow methods

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+  async headers() {
+    return [{
+      source: "/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Methods", value: "GET" },
+      ],
+    }];
+  },
+};
