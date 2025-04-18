9766c6eda79363dee35c8a7ce94d6a1a8465d8c5
AssoDePicche <samuelprado730@gmail.com>
2025-04-18 19:47:29 -0300

    feat: login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useState } from "react";
+
+import Form from "next/form";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import Alert from "../../components/alert";
+import Button from "../../components/button";
+import Input from "../../components/input";
+
+export default function Login() {
+  const [alertText, setAlertText] = useState(null);
+
+  const router = useRouter();
+
+  async function submit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
+    try {
+      const data = new FormData(event.currentTarget);
+
+      const headers = new Headers();
+
+      headers.set("Accept", "application/json");
+
+      headers.set("Accept-Language", "en-US");
+
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          email: data.get("email"),
+          password: data.get("password"),
+        })
+      }).then((response) => response.json());
+
+      if (200 !== response.status) {
+        setAlertText(response.error);
+
+        return;
+      }
+
+      localStorage.setItem("token", response.payload.token);
+
+      router.push("/home");
+    } catch (error) {
+      setAlertText(error.message);
+    }
+  }
+
+  return (
+    <Form className="flex flex-col gap-y-4" onSubmit={submit}>
+      <h1 className="font-black text-4xl">Login</h1>
+
+      <Input type="email" name="email" placeholder="Email" />
+
+      <Input type="password" name="password" placeholder="Password" />
+
+      <Button type="submit">Login</Button>
+
+      <p>
+        Already have an account? <Link href="/signup" className="underline">Sign Up</Link>
+      </p>
+
+      <Alert>{alertText}</Alert>
+    </Form>
+  );
+}
