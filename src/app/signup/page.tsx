557be134f6dbfe6ae65c7fd81a925972ce7294e3
AssoDePicche <samuelprado730@gmail.com>
2025-04-18 19:47:38 -0300

    feat: signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { useState } from "react";
+
+import Form from 'next/form';
+import Link from 'next/link';
+import { useRouter } from "next/navigation";
+
+import Alert from '../../components/alert';
+import Button from '../../components/button';
+import Input from '../../components/input';
+
+export default function Signup() {
+  const [alertText, setAlertText] = useState(null);
+
+  const router = useRouter();
+
+  async function submit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
+    try {
+      const data = new FormData(event.currentTarget);
+
+      const headers = new Headers();
+
+      headers.set("Accept", "application/json");
+
+      headers.set("Accept-Language", "en-US");
+
+      let response = await fetch("http://localhost:3000/signup", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          username: data.get("username"),
+          email: data.get("email"),
+          password: data.get("password"),
+        })
+      }).then((response) => response.json());
+
+      if (201 !== response.status) {
+        setAlertText(response.error);
+
+        return;
+      }
+
+      response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          email: data.get("email"),
+          password: data.get("password"),
+        })
+      }).then((response) => response.json());
+
+      if (200 !== response.status) {
+        setAlertText(response.error);
+
+        return;
+      }
+
+      localStorage.setItem("token", response.payload.token);
+
+      router.push("/home");
+    } catch (error) {
+      setAlertText(error.message);
+    }
+  }
+
+  return (
+    <Form className="flex flex-col gap-y-4" onSubmit={submit}>
+      <h1 className="font-black text-4xl">Sign Up</h1>
+
+      <Input type="text" name="username" placeholder="Username" />
+
+      <Input type="email" name="email" placeholder="Email" />
+
+      <Input type="Password" name="password" placeholder="Password" />
+
+      <Button type="submit">Sign Up</Button>
+
+      <p>
+        Already have an account? <Link href="/login" className="underline">Login</Link>
+      </p>
+
+      <Alert>{alertText}</Alert>
+    </Form>
+  );
+}
