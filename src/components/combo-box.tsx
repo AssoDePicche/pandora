a691b9d3a0b271bb1db832ac31d57009945d808f
AssoDePicche <samuelprado730@gmail.com>
2025-04-19 23:20:32 -0300

    feat: combo-box.tsx

---
+++ src/components/combo-box.tsx
@@ -0,0 +1,16 @@
+export const Option = ({ value, children }) => (
+  <option value={value}>{children}</option>
+);
+
+type Properties = {
+  name: string;
+  children?: React.ReactNode;
+};
+
+export default function ComboBox(properties: Properties) {
+  return (
+    <select name={properties.name} className="bg-(--background) rounded-lg border-(--component) border-2 border-solid w-full px-4 py-3 text-base focus:outline-none">
+      {properties?.children}
+    </select>
+  );
+};
