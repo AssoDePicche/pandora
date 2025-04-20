dc0a5133275b44eba7c55219ae2a3bfef2b2aae0
AssoDePicche <samuelprado730@gmail.com>
2025-04-19 23:21:55 -0300

    refactor: styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 md:flex md:items-center md:justify-between md:p-6">
-      <aside>Copyright &copy; {new Date().getFullYear()}. All rights reserved.</aside>
+    <footer className="mt-auto w-full p-4 md:flex md:items-center md:justify-between md:p-6">
+      <aside>Copyright &copy; {year}. All rights reserved.</aside>
     </footer>
   );
 }
