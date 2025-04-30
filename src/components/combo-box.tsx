a1befae6c23a83b66a4b8098e58d754113ea706f
AssoDePicche <samuelprado730@gmail.com>
2025-04-30 18:02:31 -0300

    refactor: combo-box component declaration

--- src/components/combo-box.tsx
+++ src/components/combo-box.tsx
@@ -7,7 +7,7 @@ type Properties = {
   children?: React.ReactNode;
 };
 
-export default function ComboBox(properties: Properties) {
+export default function Component(properties: Properties) {
   return (
     <select name={properties.name} className="bg-(--background) rounded-lg border-(--component) border-2 border-solid w-full px-4 py-3 text-base focus:outline-none">
       {properties?.children}
