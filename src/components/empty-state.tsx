0c7b97dacd0e5b2c14899dfa3b0987892c6e9169
AssoDePicche <samuelprado730@gmail.com>
2025-04-30 18:03:14 -0300

    refactor: empty-state component declaration

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -1,8 +1,8 @@
-type Properties = {
+type Properties = Readonly<{
   children?: React.ReactNode;
-};
+}>;
 
-export default function EmptyState(properties: Properties) {
+export default function Component(properties: Properties) {
   return (
     <div className="rounded-lg border-(--component) border-2 border-dashed h-full w-full px-4 py-4 flex items-center justify-center">
       {properties?.children}
