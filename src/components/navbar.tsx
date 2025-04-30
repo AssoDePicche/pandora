67e6a2a1e6357d1f89eec2356bd60c7de83b2de7
AssoDePicche <samuelprado730@gmail.com>
2025-04-30 18:04:11 -0300

    refactor: routes as a hashmap

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,29 @@
-import Link from "next/link"
+'use client';
 
-export default function Navbar() {
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+
+export default function Component() {
+  const pathname = usePathname();
+
+  const routes = new Map<string, string[]>([
+    ['/', ['/login', 'Sign In']],
+    ['/login', ['/signup', 'Sign Up']],
+    ['/signup', ['/login', 'Login']],
+  ]);
+
+  const href = routes.has(pathname) ? routes.get(pathname) : ['/logout', 'Sign Out'];
+
   return (
-    <header className="navbar px-4 py-4 flex">
+    <header className="navbar px-4 py-4 flex w-full">
       <div className="flex-1">
         <Link href="/" className="text-xl">Pandora</Link>
       </div>
       <nav className="flex">
         <ul className="menu menu-horizontal">
           <li>
-            <Link href="/login">Login</Link>
+            <Link href={`${href[0]}`}>{`${href[1]}`}</Link>
           </li>
         </ul>
       </nav>
