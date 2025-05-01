8aef4c0e95af89a9bf343e109cc09102beaf3075
AssoDePicche <samuelprado730@gmail.com>
2025-05-01 15:36:33 -0300

    refactor: text-field component declaration

--- src/components/text-field.tsx
+++ src/components/text-field.tsx
@@ -1,12 +1,12 @@
-type Properties = {
+type Properties = Readonly<{
   type: string;
   name: string;
   minlength?: number;
   maxlength?: number;
   placeholder?: string;
-};
+}>;
 
-export default function TextField(properties: Properties) {
+export default function Component(properties: Properties) {
   return (
     <input type={properties.type} name={properties.name} placeholder={properties?.placeholder} minLength={properties?.minlength} maxLength={properties?.maxlength} required className="rounded-lg border-(--component) border-2 border-solid w-full px-4 py-3 text-base focus:outline-none" />
   );
