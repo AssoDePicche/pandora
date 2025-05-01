19de5949f1a5db25a5ab94781a27017149233d27
AssoDePicche <samuelprado730@gmail.com>
2025-05-01 15:46:20 -0300

    refactor: remove unused imports

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,10 +4,6 @@ import Link from 'next/link';
 
 import EmptyState from '@/components/empty-state';
 
-import Icon from '@/components/icon';
-
-import Sidebar from '@/components/sidebar';
-
 import AuthGuard from '@/components/auth-guard';
 
 export default function Page() {
